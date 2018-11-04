fce0531c3bfa322c1ddcfbbea849ca9ff717b7fe
Austin C <austins.accts@gmail.com>
2018-11-04 14:36:12 -0800

    create begining class and reference for a recipe book app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { constructDependencies } from '@angular/core/src/di/reflective_provider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-recipes';
+  firstRecipe: Recipe = new Recipe('raw-beefof-the-wellingstons', [
+    new Ingredient('beef', 1),
+    new Ingredient('salsa', 3)
+  ], 'cook the beef untill it is welling-toned');
+}
+export class Recipe {
+  constructor(public title: string, public ingredients: Ingredient[], public directions: string) { }
+}
+export class Ingredient {
+  constructor(public title: string, public quantity: number) { }
 }
